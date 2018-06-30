80ab9a7e050b85dc28ed57d5e74af44cf901d219
Joshua Loyal <jloyal25@gmail.com>
2018-06-30 13:00:11 -0500

    restructured format

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/version.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/version.py
@@ -4,5 +4,5 @@ from __future__ import print_function
 
 __all__ = ['__version__', 'VERSION']
 
-__version__ = '{{cookiecutter.project_version}}'
+__version__ = '{{cookiecutter.version}}'
 VERSION = __version__
